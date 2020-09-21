24a97d7f694986068641fff18828e4549ed3aa67
Opilane TTHK <opilane@tthk.ee>
2020-09-21 15:06:11 +0300

    Strings for Loops

---
+++ AsInUserInput/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AsInUserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Whats your name?");
+            string Userinput = Console.ReadLine();
+
+            int counter = 0;
+            char searchCriteria = 'a';
+            foreach(char letter in Userinput)
+            {
+                if (letter == searchCriteria)
+                {
+                    counter = counter + 1;
+                }
+            }
+
+            if(counter > 0)
+            {
+                Console.WriteLine($"There are {counter} A's in your text.");
+            }else
+            {
+                Console.WriteLine("There are no A's in your name");
+            }
+
+           
+        }
+    }
+}
---
+++ LongerName/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LongerName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first name.");
+            string nameone = Console.ReadLine();
+            Console.WriteLine("Enter the second name.");
+            string nametwo = Console.ReadLine();
+
+            int lenghtone = 0;
+            int lenghttwo = 0;
+
+            foreach(char letter in nameone)
+            {
+                lenghtone = lenghtone + 1;
+            }
+             foreach (char letter in nametwo)
+            {
+                lenghttwo = lenghttwo + 1;
+            }
+
+            if (lenghtone > lenghttwo)
+            {
+                Console.WriteLine("The first name is longer.");
+            }else if (lenghtone < lenghttwo)
+            {
+                Console.WriteLine("The second name is longer.");
+            }else Console.WriteLine("The names are exactly as long.");
+
+
+        }
+    }
+}
